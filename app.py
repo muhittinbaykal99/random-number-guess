ef05f11bc1ac0f51d9f86d429f8f78fcfaee8348
Muhittin Baykal <91673221+muhittinbaykal99@users.noreply.github.com>
2024-09-19 14:19:11 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,22 @@ def guess(x):
         elif guess < random_number:
             print("Higher.")
         elif guess == random_number:
-            print(f"Exactly. It's {random_number}")
+            print(f"Congrats. It's {random_number}")
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess - 1
+        if feedback == 'l':
+            low = guess + 1
+    print(f'Yay! The computer guessed your number {guess} correctly!')
 
-guess(45)
+computer_guess(1000)
